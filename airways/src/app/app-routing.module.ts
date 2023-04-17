74c4d188b73f7e865fed4bcf1ab28b05b1317a5e
DashaErmolich <dasha.ermolich@gmail.com>
2023-04-17 15:41:50 +0200

    feat: add lazy loading for auth module

--- airways/src/app/app-routing.module.ts
+++ airways/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
-import { AuthPageComponent } from './auth/pages/auth-page/auth-page.component';
 
 const routes: Routes = [
   {
@@ -11,7 +10,7 @@ const routes: Routes = [
   },
   {
     path: 'auth',
-    component: AuthPageComponent,
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: 'flight',
--- airways/src/app/auth/auth.module.ts
+++ airways/src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AuthPageComponent } from './pages/auth-page/auth-page.component';
 import { LoginTabComponent } from './components/login-tab/login-tab.component';
 import { SignUpTabComponent } from './components/sign-up-tab/sign-up-tab.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { SignUpTabComponent } from './components/sign-up-tab/sign-up-tab.compone
   ],
   imports: [
     CommonModule,
+    AuthRoutingModule,
   ],
 })
 export class AuthModule { }
